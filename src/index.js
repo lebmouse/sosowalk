fa7999e47f4b6deeb36c631aa8e2b8d344d8877d
lebmouse <gtah2yk@gmail.com>
2019-09-24 01:00:19 +0900

    zeit 초기배포 성공함

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const port = process.env.PORT || 3000;
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const Multer = require("multer");
 const multerGoogleStorage = require("multer-google-storage");
 const fs = require("fs");
@@ -10,19 +9,24 @@ const { prisma } = require("./generated/prisma-client");
 const upload = Multer({
   storage: multerGoogleStorage.storageEngine({
     bucket: "smartimage",
-    projectID: "imageserver-253203",
+    projectId: "imageserver-253203",
     keyFilename: "imageserver-fc3c49a14bf7.json"
   })
 });
 
 const app = express();
+app.use(express.json())
 
-app.use(bodyParser.json());
-
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`> Ready On Server http://localhost:${port}`);
+});
 app.get("/", async (req, res) => {
   res.send("api 서버");
 });
-
+app.get("/hello", async(req,res)=>{
+  res.send("hello world")
+})
 app.post("/upload", upload.single("file"), (req, res) => {
   console.log(req.file);
   res.json({ url: req.file.path });
@@ -40,7 +44,3 @@ app.post("/upload", upload.single("file"), (req, res) => {
   // }
 });
 
-app.listen(port, err => {
-  if (err) throw err;
-  console.log(`> Ready On Server http://localhost:${port}`);
-});
